bf29b820e66b69baee3c98afd4ebb91d2d1f9bb0
KO(Rpc) <keisuke.otaki.jp@gmail.com>
2022-04-22 14:23:22 +0900

    2次元でのランダム位置プロット，遷移行列の雑なプロット

--- src/build_trans_mat.py
+++ src/build_trans_mat.py
@@ -45,24 +45,71 @@ def read_data(path="./data"):
     dict_wd_index = {day: np.where(weekday == day)[0] for day in list_days}
     print(dict_wd_index[0])
 
-    for target in list_days:
-        train, test = train_test_split(dict_wd_index[target], test_size=0.25)
-        train.sort()
-        test.sort()
-        H = Zr["incidence_matrices"][train]
-        print(f" Train: {train}")
-        print(f" Test : {test}")
-        T = build_matrix(H, Zs, train, test, n_stop)
+    # for target in list_days:
+    #     train, test = train_test_split(dict_wd_index[target], test_size=0.25)
+    #     train.sort()
+    #     test.sort()
+    #     H = Zr["incidence_matrices"][train]
+    #     print(f" Train: {train}")
+    #     print(f" Test : {test}")
+    #     T = build_matrix(H, Zs, train, test, n_stop)
 
-        # visualize
-        f = plt.figure()
-        a = f.gca()
-        a.imshow(T, cmap="jet", aspect=1)
-        a.set_xlabel("Index of stops")
-        a.set_ylabel("Index of stops")
-        plt.tight_layout()
-        plt.savefig(f"figures/example_trans_mat_{target}.png")
-        plt.close()
+    #     # visualize
+    #     f = plt.figure()
+    #     a = f.gca()
+    #     a.imshow(T, cmap="jet", aspect=1)
+    #     a.set_xlabel("Index of stops")
+    #     a.set_ylabel("Index of stops")
+    #     plt.tight_layout()
+    #     plt.savefig(f"figures/example_trans_mat_{target}.png")
+    #     plt.close()
+
+    target = 0
+    train, test = train_test_split(dict_wd_index[target], test_size=0.25)
+    train.sort()
+    test.sort()
+    H = Zr["incidence_matrices"][train]
+    print(f" Train: {train}")
+    print(f" Test : {test}")
+    T = build_matrix(H, Zs, train, test, n_stop)
+
+    # # visualize
+    # f = plt.figure()
+    # a = f.gca()
+    # a.imshow(T, cmap="jet", aspect=1)
+    # a.set_xlabel("Index of stops")
+    # a.set_ylabel("Index of stops")
+    # plt.tight_layout()
+    # plt.savefig(f"figures/example_trans_mat_{target}.png")
+    # plt.close()
+
+    # test 0
+    T0 = Zr["incidence_matrices"][test[0]]
+    used_stops = np.where(T0.sum(axis=1) > 0)[0]
+    num_used_stops = len(used_stops)
+    print(T0.shape)
+    print(used_stops)
+
+    XY = np.random.rand(num_used_stops, 2)
+    f = plt.figure(figsize=(10, 5))
+    a1 = f.add_subplot(1, 2, 1)
+    a2 = f.add_subplot(1, 2, 2)
+    a1.scatter(x=XY[:, 0], y=XY[:, 1])
+    a2.scatter(x=XY[:, 0], y=XY[:, 1])
+
+    for i in range(num_used_stops):
+        ni = used_stops[i]
+        for j in range(num_used_stops):
+            nj = used_stops[j]
+            if T0[ni, nj] > 0:
+                a1.plot([XY[i, 0], XY[j, 0]], [
+                    XY[i, 1], XY[j, 1]], "k--", alpha=0.3)
+            a2.plot([XY[i, 0], XY[j, 0]], [
+                    XY[i, 1], XY[j, 1]], "k--", lw=T[i, j] * 3, alpha=0.3)
+
+    plt.tight_layout()
+    plt.savefig("figures/route.png")
+    plt.close()
 
 
 def build_trans_mat():
